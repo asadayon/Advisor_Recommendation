a1dcc859fdd7b35788b6a7e370fda73e72fa2ea9
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-07-08 06:54:38 -0400

    Database Detached temporarily

--- Rec2.py
+++ Rec2.py
@@ -26,7 +26,7 @@ def insert_message(connection, role, content):
     cursor = connection.cursor()
     cursor.execute("INSERT INTO chat_messages (role, content) VALUES (%s, %s)", (role, content))
     connection.commit()
-***    
+"""    
 def connect_to_db():
     return mysql.connector.connect(
         host=st.secrets["HOST"],
@@ -35,7 +35,7 @@ def connect_to_db():
         password=st.secrets["PASSWORD"],
         database=st.secrets["DATABASE"]  # Replace with your actual database name
     )        
-***        
+"""       
 
 def tokenize(txt):
   txt=str(txt)
