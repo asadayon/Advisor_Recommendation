319f874ed4f36743f9d6957034a47a4f84088c92
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-05 08:38:24 -0400

    Update grad_adv_rec.py

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -287,7 +287,7 @@ for key, default in [
 if 'clicked' not in st.session_state:
     st.session_state.clicked = False
 
-questions=["How does the system work?","Tell me how the first advisor in text similarity model recommended?","Tell me how the first advisor in text similarity model recommended?"]
+questions=["How does the system work?","Tell me how the first advisor in text similarity model recommended?","Tell me how the first advisor in text similarity model recommended?","","",""]
 data_dict={}
 flag=0
 st.session_state["openai_model"] = "gpt-3.5-turbo"
