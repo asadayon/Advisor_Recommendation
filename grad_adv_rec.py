40c5328b7c8583efbbf0064a349d1e0e665379fc
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-06 07:54:25 -0400

    Update grad_adv_rec.py

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -375,7 +375,7 @@ elif st.session_state.page == "v1" or st.session_state.page == "v2":
         scenario = st.session_state.selected_scenarios[1]
         st.info(scenario)
         st.markdown("Enter keywords of your reseach interest separated by comma and get system's recommendations.")
-        keywords = st.multiselect("Select symptoms:", options=options)
+        keywords = st.multiselect("Select Research Keywords:", options=options)
     if st.session_state.page == "v2":
         st.title("Grad Student Advisor Recommender System")
         st.subheader("Version 2 - Recommendation with AI Chatbot")
@@ -385,7 +385,7 @@ elif st.session_state.page == "v1" or st.session_state.page == "v2":
         scenario = st.session_state.selected_scenarios[2]
         st.info(scenario)
         st.markdown("Enter keywords of your reseach interest separated by comma and get system's recommendations.")
-        keywords = st.multiselect("Select symptoms:", options=options)
+        keywords = st.multiselect("Select Research Keywords:", options=options)
         
 
     if st.button("Predict"):
