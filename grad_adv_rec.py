e584ba5f88f5e05780c389de0e896b448d20c47f
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-22 08:47:25 -0400

    Update grad_adv_rec.py

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -353,7 +353,7 @@ def render_v2_quiz_flow(questions, idx, scenario):
         "Select your answer:",
         options,
         index=None,
-        key=radio_key
+        key="hi"
     )
     if selected:
             st.write(f"You selected: {selected}")
