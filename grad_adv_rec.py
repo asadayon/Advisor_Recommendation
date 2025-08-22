5f6f4ce1b910bee44bc6fe4f0500f5b085378876
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-22 06:31:12 -0400

    Update grad_adv_rec.py

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -351,7 +351,7 @@ def render_v2_quiz_flow(questions, idx, scenario):
         key=radio_key
     )
     if selected:
-            st.write("Hi")
+            st.write(selected)
     if selected != prev_selected:
         st.session_state[selected_option_key] = selected
         chosen_index = options.index(selected) + 1 if selected in options else None
@@ -379,7 +379,7 @@ def render_v2_quiz_flow(questions, idx, scenario):
 
     correct = question['answer']
     chosen = options.index(selected) + 1 if selected in options else None
-
+    st.write(chosen)
     if chosen:
         if chosen == correct:
             st.success("✅ Correct!")
