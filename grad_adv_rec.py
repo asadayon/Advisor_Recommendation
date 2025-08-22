fdb8fd59dc4022b09b67969386e1770a7f369294
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-22 09:09:48 -0400

    Update grad_adv_rec.py

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -350,6 +350,7 @@ def render_v2_quiz_flow(questions, idx, scenario):
     st.write("All session state keys:", list(st.session_state.keys()))
     st.markdown("__*Please select one of the options to know more about it.*__")
     options = ["Option 1", "Option 2", "Option 3"]
+    radio_key='hi'
     selected = st.radio(
         "Select your answer:",
         options,
