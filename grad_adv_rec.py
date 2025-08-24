c52ca9fec418b2266c38d1c95b6b50b155b98fd1
Md Ashaduzzaman <102450075+asadayon@users.noreply.github.com>
2025-08-23 22:41:22 -0400

    Update grad_adv_rec.py

--- grad_adv_rec.py
+++ grad_adv_rec.py
@@ -380,6 +380,8 @@ def render_v2_quiz_flow(questions, idx, scenario):
 
     correct = question['answer']
     chosen = options.index(selected) + 1 if selected in options else None
+    st.write(correct)
+    st.write(chosen)
 
     if chosen:
         if chosen == correct:
